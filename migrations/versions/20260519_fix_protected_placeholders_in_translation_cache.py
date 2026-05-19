15d60b87604db25fd48025f6194c35ea0740a2c5
Vladislav Spivak <vlad@helloflare.com>
2026-05-19 22:19:48 +0300

    retranslate

---
+++ migrations/versions/20260519_fix_protected_placeholders_in_translation_cache.py
@@ -0,0 +1,40 @@
+"""Fix broken {PROTECTED N} placeholders in translation cache tables
+
+Revision ID: 20260519_fix_protected_placeholders
+Revises: 20260518_1506_fix_content_view_content_id_type
+Create Date: 2026-05-19 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '20260519_fix_protected_placeholders'
+down_revision = '20260518_1506_fix_content_view_content_id_type'
+branch_labels = None
+depends_on = None
+
+# Matches {PROTECTED 0}, {PROTECTED_0}, {PROTECTED 1}, {PROTECTED_1}, etc.
+_PATTERN = r'\{PROTECTED[_ ]\d+\}'
+_REPLACEMENT = 'Yonca'
+
+
+def _fix_table(table_name: str) -> None:
+    t = sa.table(table_name, sa.column('translated_text', sa.Text()))
+    op.get_bind().execute(
+        sa.update(t)
+        .where(t.c.translated_text.op('~')(_PATTERN))
+        .values(
+            translated_text=sa.func.regexp_replace(
+                t.c.translated_text, _PATTERN, _REPLACEMENT, 'g'
+            )
+        )
+    )
+
+
+def upgrade():
+    _fix_table('translation')
+    _fix_table('content_translation')
+
+
+def downgrade():
+    pass
